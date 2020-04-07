8510907aaa0bf7610b4754b1b42c764cb16fe94d
Cabomaldade <soutom@yahoo.com.br>
2020-04-07 13:49:05 -0300

    tree sort exercise two

--- src/com/cabomaldade/coursera/UniformRandom.java
+++ src/com/cabomaldade/coursera/UniformRandom.java
@@ -1,35 +1,30 @@
 package com.cabomaldade.coursera;
 
 /* Solution by André Vasconcelos Souto
-    1.2.30 Uniform random numbers.
-    Write a program that prints five uniform random numbers
-    between 0 and 1, their average value, and their minimum
-    and maximum values. Use Math.random(), Math.min(), and Math.max().
+    1.2.34 Three-sort. Write a program that takes three
+    integer command-line arguments and prints them in ascending order.
+    Use Math.min() and Math.max().
  */
 
 public class UniformRandom {
     public static void main(String[] args) {
-        double[] uniformes = new double[5];
 
-        double avg = 0.0;
-        double min = 1.0;
-        double max = 0.0;
+        int arg1 = Integer.parseInt(args[0]);
+        int arg2 = Integer.parseInt(args[1]);
+        int arg3 = Integer.parseInt(args[2]);
 
-        for (int i=0; i<= uniformes.length -1; i++) {
-            uniformes[i] = Math.random();
-            avg += uniformes[i];
-            min = Math.min(min, uniformes[i]);
-            max = Math.max(max, uniformes[i]);
+        int min = Math.min(Math.min(arg1, arg2),arg3);
+        int max = Math.max(Math.max(arg1, arg2),arg3);
+        int medium;
+
+        if (arg1 != min && arg1 != max){
+            medium = arg1;
+        } else if (arg2 != min && arg2 != max){
+            medium = arg2;
+        } else {
+            medium = arg3;
         }
 
-        for (int i=0; i<= uniformes.length -1; i++) {
-            System.out.println("Number " + i + " = " + uniformes[i]);
-            if (i == uniformes.length -1){
-                System.out.println("Average :" + avg/uniformes.length);
-                System.out.println("Minimum :" + min);
-                System.out.println("Maximum :" + max);
-            }
-        }
-
+        System.out.println(min + " - " + medium + " - " + max);
     }
 }
